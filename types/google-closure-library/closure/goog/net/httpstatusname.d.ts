1952dee102a0b7cdfa012f81da0b8b6fba325625
Ty Lewis <ty@marq.com>
2023-11-01 10:35:05 -0600

    Add types for goog.net.HttpStatusName (PRESS-19699)

---
+++ types/google-closure-library/closure/goog/net/httpstatusname.d.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../../globals.d.ts"/>
+
+declare module "goog:goog.net.HttpStatusName" {
+	import alias = goog.net.HttpStatusName;
+	export default alias;
+}
+
+declare namespace goog.net {
+	/**
+	 * HTTP Status Code Names defined in RFC 2616 and RFC 6585.
+	 * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+	 * @see http://tools.ietf.org/html/rfc6585
+	 * @type {!Object<number, string>}
+	 */
+	let HttpStatusName: { [key: number]: string };
+}
